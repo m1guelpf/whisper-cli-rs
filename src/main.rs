9f47cafb83aece808e087aeeb2ee2e4175e2f9e2
Miguel Piedrafita <github@miguelpiedrafita.com>
2022-12-28 14:58:28 +0100

    Improve CLI

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,10 @@ use crate::{
     model::{Model, Size},
     whisper::Whisper,
 };
+use clap::Parser;
 use std::path::Path;
 use utils::write_to;
+use whisper::Language;
 
 mod ffmpeg_decoder;
 mod model;
@@ -13,15 +15,40 @@ mod transcript;
 mod utils;
 mod whisper;
 
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Name of the Whisper model to use
+    #[clap(short, long, default_value = "medium")]
+    model: Size,
+
+    /// Language spoken in the audio. Attempts to auto-detect by default.
+    #[clap(short, long, default_value = "auto")]
+    lang: Language,
+
+    /// Path to the audio file to transcribe
+    audio: String,
+}
+
 #[tokio::main]
 async fn main() {
-    let args = std::env::args().collect::<Vec<String>>();
-    assert!(args.len() == 2, "Usage: rs-whisper <audio_file>");
-
-    let audio = Path::new(&args[1]);
+    let mut args = Args::parse();
+    let audio = Path::new(&args.audio);
     let file_name = audio.file_name().unwrap().to_str().unwrap();
 
-    let mut whisper = Whisper::new(Model::new(Size::Medium)).await;
+    if !audio.exists() {
+        panic!("The provided audio file does not exist.");
+    }
+
+    if args.model.is_english_only() && args.lang == Language::Auto {
+        args.lang = Language::English;
+    }
+
+    if args.model.is_english_only() && args.lang != Language::English {
+        panic!("The selected model only supports English.");
+    }
+
+    let mut whisper = Whisper::new(Model::new(args.model), args.lang).await;
     let transcript = whisper.transcribe(audio).unwrap();
 
     write_to(
--- src/model.rs
+++ src/model.rs
@@ -2,18 +2,65 @@ use crate::utils::download_file;
 use dirs::cache_dir;
 use std::{fmt::Display, fs, path::PathBuf};
 
-#[allow(dead_code)]
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, clap::ValueEnum)]
 pub enum Size {
+    #[clap(name = "tiny.en")]
     TinyEnglish,
+    #[clap(name = "tiny")]
     Tiny,
+    #[clap(name = "base.en")]
     BaseEnglish,
+    #[clap(name = "base")]
     Base,
+    #[clap(name = "small.en")]
     SmallEnglish,
+    #[clap(name = "small")]
     Small,
+    #[clap(name = "medium.en")]
     MediumEnglish,
+    #[clap(name = "medium")]
     Medium,
+    #[clap(name = "large")]
     Large,
+    #[clap(name = "large-v1")]
     LargeV1,
+}
+
+impl Size {
+    pub fn get_path(&self) -> PathBuf {
+        let mut path = cache_dir().expect("Could not find cache directory");
+        path.push("whisper");
+        path.push("models");
+        path.push(format!("{self}.bin"));
+
+        path
+    }
+
+    pub fn is_english_only(&self) -> bool {
+        matches!(
+            self,
+            Self::TinyEnglish | Self::BaseEnglish | Self::SmallEnglish | Self::MediumEnglish
+        )
+    }
+}
+
+impl Display for Size {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let key = match self {
+            Self::TinyEnglish => "tiny.en",
+            Self::Tiny => "tiny",
+            Self::BaseEnglish => "base.en",
+            Self::Base => "base",
+            Self::SmallEnglish => "small.en",
+            Self::Small => "small",
+            Self::MediumEnglish => "medium.en",
+            Self::Medium => "medium",
+            Self::Large => "large",
+            Self::LargeV1 => "large-v1",
+        };
+
+        write!(f, "{key}")
+    }
 }
 
 pub struct Model {
@@ -48,35 +95,5 @@ impl Model {
             path.to_str().unwrap(),
         )
         .await;
-    }
-}
-
-impl Size {
-    pub fn get_path(&self) -> PathBuf {
-        let mut path = cache_dir().expect("Could not find cache directory");
-        path.push("whisper");
-        path.push("models");
-        path.push(format!("{self}.bin"));
-
-        path
-    }
-}
-
-impl Display for Size {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let key = match self {
-            Self::TinyEnglish => "tiny.en",
-            Self::Tiny => "tiny",
-            Self::BaseEnglish => "base.en",
-            Self::Base => "base",
-            Self::SmallEnglish => "small.en",
-            Self::Small => "small",
-            Self::MediumEnglish => "medium.en",
-            Self::Medium => "medium",
-            Self::Large => "large",
-            Self::LargeV1 => "large-v1",
-        };
-
-        write!(f, "{key}")
     }
 }
