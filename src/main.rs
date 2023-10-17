c11293ad86abea6b534757b3b63044a675f15641
tiero <3596602+tiero@users.noreply.github.com>
2023-10-18 01:34:46 +0200

    Split into transcribe and serve commands

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,51 @@
 #![warn(clippy::all, clippy::pedantic, clippy::nursery)]
 
 use clap::Parser;
+use serde::Serialize;
 use std::path::Path;
-use utils::write_to;
+use warp::{self, Filter};
+use whisper_cli::{Language, Model, Size, Whisper};
+
+use crate::utils::write_to;
 
 mod utils;
 
-use whisper_cli::{Language, Model, Size, Whisper};
+#[derive(Serialize)]
+struct TranscriptionResponse {
+    text: String,
+}
+
+/* 
+#[derive(Deserialize)]
+struct TranscriptionRequest {
+    model: Option<String>,
+    language: Option<String>,
+    prompt: Option<String>,
+    response_format: Option<String>,
+    temperature: Option<f32>,
+} 
+*/
 
 #[derive(Parser)]
-#[command(
-    author,
-    version,
-    about = "Locally transcribe audio files, using Whisper.",
-    long_about = "Generate a transcript of an audio file using the Whisper speech-to-text engine. The transcript will be saved as a .txt, .vtt, and .srt file in the same directory as the audio file."
-)]
-struct Args {
+struct Opts {
+    #[clap(subcommand)]
+    subcmd: SubCommand,
+}
+
+#[derive(Parser)]
+enum SubCommand {
+    #[command(about = "Start the transcription server.")]
+    Serve {
+        /// Port to listen on
+        #[clap(short, long, default_value = "3030")]
+        port: u16,
+    },
+    #[command(about = "Transcribe a given audio file.")]
+    Transcribe(TranscribeArgs),
+}
+
+#[derive(Parser)]
+struct TranscribeArgs {
     /// Name of the Whisper model to use
     #[clap(short, long, default_value = "medium")]
     model: Size,
@@ -25,6 +55,7 @@ struct Args {
     lang: Option<Language>,
 
     /// Path to the audio file to transcribe
+    #[clap(name = "AUDIO")]
     audio: String,
 
     /// Toggle translation
@@ -34,11 +65,53 @@ struct Args {
     /// Generate timestamps for each word
     #[clap(short, long, default_value = "false")]
     karaoke: bool,
+
+    /// Write transcription results to .txt, .vtt, and .srt files.
+    #[clap(short, long, default_value = "false")]
+    write: bool,
 }
 
 #[tokio::main]
 async fn main() {
-    let mut args = Args::parse();
+    for arg in std::env::args() {
+        println!("{}", arg);
+    }
+    let opts = Opts::parse();
+    match opts.subcmd {
+        SubCommand::Serve { port } => start_server(port).await,
+        SubCommand::Transcribe(args) => transcribe_audio(args).await,
+    }
+}
+
+async fn start_server(port: u16) {
+    // Define the API endpoint
+    let transcription = warp::post()
+        .and(warp::path("v1"))
+        .and(warp::path("audio"))
+        .and(warp::path("transcriptions"))
+        .and(warp::multipart::form())
+        .and_then(handle_transcription);
+
+    let routes = transcription;
+
+    warp::serve(routes).run(([0, 0, 0, 0], port)).await;
+}
+
+async fn handle_transcription(
+    form: warp::multipart::FormData,
+) -> Result<impl warp::Reply, warp::Rejection> {
+    // Handle the uploaded file and parameters from the multipart form.
+    // For simplicity, I'm providing a placeholder response here.
+    // You'd replace this with the actual transcription logic.
+    Ok(warp::reply::json(&format!(
+        "{{
+          \"text\": \"Transcription placeholder for uploaded audio.\"
+      }}"
+    )))
+}
+
+async fn transcribe_audio(mut args: TranscribeArgs) {
+    // Your previous CLI functionality here
     let audio = Path::new(&args.audio);
     let file_name = audio.file_name().unwrap().to_str().unwrap();
 
@@ -57,19 +130,23 @@ async fn main() {
     let transcript = whisper
         .transcribe(audio, args.translate, args.karaoke)
         .unwrap();
-
-    write_to(
-        audio.with_file_name(format!("{file_name}.txt")),
-        &transcript.as_text(),
-    );
-    write_to(
-        audio.with_file_name(format!("{file_name}.vtt")),
-        &transcript.as_vtt(),
-    );
-    write_to(
-        audio.with_file_name(format!("{file_name}.srt")),
-        &transcript.as_srt(),
-    );
-
     println!("time: {:?}", transcript.processing_time);
+
+    if args.write {
+        write_to(
+            audio.with_file_name(format!("{file_name}.txt")),
+            &transcript.as_text(),
+        );
+        write_to(
+            audio.with_file_name(format!("{file_name}.vtt")),
+            &transcript.as_vtt(),
+        );
+        write_to(
+            audio.with_file_name(format!("{file_name}.srt")),
+            &transcript.as_srt(),
+        );
+    } else {
+        println!("");
+        println!("🔊 {}", transcript.as_text());
+    }
 }
